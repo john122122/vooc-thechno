dea76704d70be3f2ee6f74ec15b94c0d262ece62
abbas usman <johnmccain2244@gmail.com>
2023-12-19 11:36:08 +0900

    I created the express part of the project

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+//const router = require("./router.js");
+//const router_bssr = require("./router_bssr.js");
+//const cookieParser = require("cookie-parser");
+
+//let session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URL,
+    collection: "sessions",
+});
+
+// 1: Entering code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// 2: Session code
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         cookie: {
+//             maxAge: 1000 * 60 * 30, // for 30 minutes
+//         },
+//         store: store,
+//         resave: true,
+//         saveUninitialized: true,
+//     })
+// );
+// app.use(function(req, res, next) {
+//     res.locals.member = req.session.member;
+//     next();
+// });
+
+// 3 Views codelari
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.use("/techno", router_bssr);   // traditional usul
+app.use("/", router);             // react usul
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const mongoose = require("mongoose");
+
+const connectionString = process.env.MONGO_URL;
+mongoose.set("strictQuery",false);
+
+mongoose.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, goose) => {
+        if (err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            // console.log(goose);   // buyerda mongoose clientni beradi.
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = process.env.PORT || 3003;
+            server.listen(PORT, function () {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+            );
+        });
+    }
+});
+
+
